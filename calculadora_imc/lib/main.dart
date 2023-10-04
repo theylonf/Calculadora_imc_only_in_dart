7cc2e8325596cb23bcbff43188253bac9c3e2565
Theylon <theylonfeitosa@gmail.com>
2023-10-04 07:54:53 -0300

    Desafio - Salvando os dados localmente com hive

--- calculadora_imc/lib/main.dart
+++ calculadora_imc/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:calculadora_imc/model/imc.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDirectory = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
+  await Hive.initFlutter();
+  await Hive.openBox('resultado_antigo');
+
   runApp(MyApp());
 }
 
@@ -10,7 +18,7 @@ class InputForm extends StatelessWidget {
   final TextEditingController alturaController;
   final Function(double, double) onCalcularPressed;
 
-  InputForm({
+  const InputForm({super.key,
     required this.pesoController,
     required this.alturaController,
     required this.onCalcularPressed,
@@ -44,29 +52,33 @@ class InputForm extends StatelessWidget {
 }
 
 class ResultadosAnteriores extends StatelessWidget {
-  final List<Map<String, dynamic>> resultados;
-
-  ResultadosAnteriores({required this.resultados});
-
   @override
   Widget build(BuildContext context) {
+    final Box _resultadoBox = Hive.box('resultado_antigo');
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const Text('Resultados Anteriores:', style: TextStyle(fontSize: 18)),
-        ListView.builder(
-          shrinkWrap: true,
-          itemCount: resultados.length,
-          itemBuilder: (context, index) {
-            double valorIMC = resultados[index]['imc'];
-            String classificacao = resultados[index]['classificacao'];
-            double peso = resultados[index]['peso'];
-            double altura = resultados[index]['altura'];
+        WatchBoxBuilder(
+          box: _resultadoBox,
+          builder: (context, box) {
+            return ListView.builder(
+              shrinkWrap: true,
+              itemCount: box.length,
+              itemBuilder: (context, index) {
+                final resultado = box.getAt(index) as Map<dynamic, dynamic>;
+                final resultadoConvertido = Map<String, dynamic>.from(resultado);
+                double valorIMC = resultadoConvertido['imc'];
+                String classificacao = resultadoConvertido['classificacao'];
+                double peso = resultadoConvertido['peso'];
+                double altura = resultadoConvertido['altura'];
 
-            return ListTile(
-              title: Text('Peso: $peso kg - Altura: $altura m'),
-              subtitle: Text(
-                  'IMC: ${valorIMC.toStringAsFixed(2)} - Classificação: $classificacao'),
+                return ListTile(
+                  title: Text('Peso: $peso kg - Altura: $altura m'),
+                  subtitle: Text('IMC: ${valorIMC.toStringAsFixed(2)} - Classificação: $classificacao'),
+                );
+              },
             );
           },
         ),
@@ -83,7 +95,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   TextEditingController pesoController = TextEditingController();
   TextEditingController alturaController = TextEditingController();
-  List<Map<String, dynamic>> resultadosAnteriores = [];
 
   @override
   Widget build(BuildContext context) {
@@ -105,22 +116,21 @@ class _MyAppState extends State<MyApp> {
                     String classificacao = imc.getClassificacao();
                     double valorIMC = imc.calcular();
 
-                    setState(() {
-                      resultadosAnteriores.add({
-                        'imc': valorIMC,
-                        'classificacao': classificacao,
-                        'peso': peso,
-                        'altura': altura,
-                      });
-                    });
+                    final resultado = {
+                      'imc': valorIMC,
+                      'classificacao': classificacao,
+                      'peso': peso,
+                      'altura': altura,
+                    };
+
+                    _saveResultado(resultado);
 
                     showDialog(
                       context: context,
                       builder: (context) {
                         return AlertDialog(
                           title: const Text('Resultado'),
-                          content: Text(
-                              'Seu IMC é: ${valorIMC.toStringAsFixed(2)}\nClassificação: $classificacao'),
+                          content: Text('Seu IMC é: ${valorIMC.toStringAsFixed(2)}\nClassificação: $classificacao'),
                           actions: <Widget>[
                             TextButton(
                               onPressed: () {
@@ -132,14 +142,15 @@ class _MyAppState extends State<MyApp> {
                         );
                       },
                     );
+
+                    setState(() {});
                   } else {
                     showDialog(
                       context: context,
                       builder: (context) {
                         return AlertDialog(
                           title: const Text('Erro'),
-                          content: const Text(
-                              'Peso e altura devem ser maiores que zero.'),
+                          content: const Text('Peso e altura devem ser maiores que zero.'),
                           actions: <Widget>[
                             TextButton(
                               onPressed: () {
@@ -155,11 +166,16 @@ class _MyAppState extends State<MyApp> {
                 },
               ),
               const SizedBox(height: 16.0),
-              ResultadosAnteriores(resultados: resultadosAnteriores),
+              ResultadosAnteriores(),
             ],
           ),
         ),
       ),
     );
+  }
+
+  void _saveResultado(Map<String, dynamic> resultado) {
+    final box = Hive.box('resultado_antigo');
+    box.add(resultado);
   }
 }
